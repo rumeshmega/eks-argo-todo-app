f308d54565ec86d57a30fa997eda265e636c635f
Chandika S <97661137+chandika-s@users.noreply.github.com>
2024-09-19 23:44:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    10WeeksofCloudOps - TODO LIST
+                    Demo - TODO LIST
                 </Row>
   
                 <hr />
